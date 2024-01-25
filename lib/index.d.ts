f14d21a4e5bbcbc08ec559d560fc88b53ae5f98c
danielreeder <danielreeder123@gmail.com>
2024-01-24 23:23:51 -0800

    test

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the purpletheme.js extension.
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
